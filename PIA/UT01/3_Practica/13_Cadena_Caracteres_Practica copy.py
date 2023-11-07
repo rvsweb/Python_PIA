95bf2854a9596c0ab10095773de0df6901cd02cc
rvsweb <siguelasreglas@gmail.com>
2023-11-07 22:05:16 +0100

    13 - Hacer Tareas de clase 3

---
+++ PIA/UT01/3_Practica/13_Cadena_Caracteres_Practica copy.py
@@ -0,0 +1,11 @@
+# Cadena Original
+cadena = "Hola Mundo"
+# Nueva cadena independiente de la original
+nueva_cadena = "a" + cadena[1:]
+print(nueva_cadena , " "  , "Hola")
+
+# Mayusculas
+print(cadena.upper())
+
+
+
